a4d17b3d3a334fb43e860a255ddd693777a9d99a
Sergey <shesergey2001@gmail.com>
2021-05-21 10:30:21 +1000

    100%

--- Multithreading/Source.cpp
+++ Multithreading/Source.cpp
@@ -10,12 +10,14 @@
 #include <mutex>
 #include<ctime>
 #include<chrono>
+#include <iomanip>
+
 
 using namespace std;
 using namespace chrono;
-
 void ToSumEven(vector<int> arr, int& sum) // Функция потока, суммирующая элементы массива.
-{                                   // Результат записывается в переменную sum, передаваемую по ссылке
+{   
+    // Результат записывается в переменную sum, передаваемую по ссылке
     int result = 0;
     for (auto element : arr)
         if (element % 2 == 0)
@@ -26,51 +28,57 @@ void ToSumEven(vector<int> arr, int& sum) // Функция потока, суммирующая элемент
 int main()
 {
     srand(time(nullptr));
-    int vectorNumber; // Число строк матрицы
-    int vectorSize; // Число столбцов
+    int vectorRows; // Число строк матрицы
+    int vectorColumns; // Число столбцов
 
     cout << "Count of rows: " << endl;
-    cin >> vectorNumber;
+    cin >> vectorRows;
 
     cout << "Count of columns" << endl;
-    cin >> vectorSize;
+    cin >> vectorColumns;
 
     cout << "MAX of my threads: " <<thread::hardware_concurrency()<<endl;
 
-    // Создаём матрицу - вектор из весторов
-    vector<vector<int> > matrix(vectorNumber);
+
+    vector<vector<int> > matrix(vectorRows);
     for (auto & element : matrix)
-    {
-        element = vector<int>(vectorSize);
-    }
+        element = vector<int>(vectorColumns);
+    
 
-    // Заполняем матрицу значениями
-    for (int i = 0; i < vectorNumber; i++)
-    {
-        for (int j = 0; j < vectorSize; j++)
-        {
+
+    for (int i = 0; i < vectorRows; ++i)
+        for (int j = 0; j < vectorColumns; ++j)
             matrix[i][j] = rand()%100;
-        }
+        
+    for (int i = 0; i < vectorRows; ++i)
+    {
+        for (int j = 0; j < vectorColumns; ++j)
+            cout << matrix[i][j]<<" ";
+        cout << endl;
     }
 
 
-    vector<int> vectorOfSum(vectorNumber); //вектор для хранения сумм строк матрицы
-    vector<thread> vectorOfThreads(vectorNumber); //вектор потоков
-    // создаём потоки
-    for (int i = 0; i < vectorNumber; i++)
+
+
+    vector<int> vectorOfSum(vectorRows); 
+    vector<thread> vectorOfThreads(vectorRows); 
+
+    for (int i = 0; i < vectorRows; ++i)
     {
         vectorOfThreads[i] = thread(ToSumEven, matrix[i], ref(vectorOfSum[i]));
         cout <<"THREAD ID: "<< vectorOfThreads[i].get_id()<<endl;
     }
 
-    // ждём завершения всех потоков
-    for (int i = 0; i < vectorNumber; ++i)
+    //this_thread::sleep_for(microseconds(2000));
+
+
+    for (int i = 0; i < vectorRows; ++i)
         vectorOfThreads[i].join();
     
 
-    // выводим результат
-    for (int i = 0; i < vectorNumber; ++i)
-        cout <<i<<" row: "<< vectorOfSum[i] << "\n";
+
+    for (int i = 0; i < vectorRows; ++i)
+        cout <<i<<" row: "<<vectorOfSum[i] <<" evens"<< "\n";
     
 
     return 0;
